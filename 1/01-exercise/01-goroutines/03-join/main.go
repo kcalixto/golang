e483c994e59e14dae88e01e528e6e93d3cdc548a
kcalixto <kauacalixtocontato@gmail.com>
2022-10-09 00:16:47 -0300

    01.03

--- 1/01-exercise/01-goroutines/03-join/main.go
+++ 1/01-exercise/01-goroutines/03-join/main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 	//TODO: modify the program
 	// to print the value as 1
 	// deterministically.
-
+	var wg sync.WaitGroup
+	wg.Add(1)
 	var data int
 
 	go func() {
+		defer wg.Done()
 		data++
 	}()
 
+	wg.Wait()
 	fmt.Printf("the value of data is %v\n", data)
 
 	fmt.Println("Done..")
