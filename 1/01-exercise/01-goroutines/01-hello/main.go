1d84dd6b0e171379908a38b26619bdde54eda2ad
kcalixto <kauacalixtocontato@gmail.com>
2022-10-08 23:44:44 -0300

    ex 01.01

--- 1/01-exercise/01-goroutines/01-hello/main.go
+++ 1/01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,20 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+	go fun("aloha!");
 
 	// goroutine with anonymous function
+	go func() {
+		fun("heyhey");
+	}()
 
 	// goroutine with function value call
+	fv := fun
+	go fv("oh no");
 
 	// wait for goroutines to end
+	fmt.Println("wait for goroutines...")
+	time.Sleep(100 * time.Millisecond)
 
 	fmt.Println("done..")
 }
